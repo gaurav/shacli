091371873e6c7751508b400ec4771f031260000b
Gaurav Vaidya <gaurav@renci.org>
2020-01-28 18:00:22 -0500

    Improved command line and outputs of error messages.

--- build.sbt
+++ build.sbt
@@ -15,6 +15,9 @@ libraryDependencies ++= {
     "com.typesafe.scala-logging"  %% "scala-logging"          % "3.9.2",
     "ch.qos.logback"              %  "logback-classic"        % "1.2.3",
 
+    // Command line argument parsing.
+    "org.rogach"                  %% "scallop"                % "3.3.2",
+
     // Import a SHACL library.
     "org.topbraid"                % "shacl"                   % "1.3.0",
 
--- src/main/scala/org/renci/shacli/ShacliApp.scala
+++ src/main/scala/org/renci/shacli/ShacliApp.scala
@@ -4,13 +4,13 @@ import java.io.{File, FileWriter, PrintWriter}
 import java.time.ZonedDateTime
 import java.util.Calendar
 
-import java.io.{File, FileInputStream, IOException, InputStream, ByteArrayOutputStream}
+import java.io.{File, FileInputStream, IOException, InputStream, ByteArrayOutputStream, StringWriter}
 
 import org.topbraid.shacl.validation._
 import org.apache.jena.ontology.OntDocumentManager
 import org.apache.jena.ontology.OntModel
 import org.apache.jena.ontology.OntModelSpec
-import org.apache.jena.rdf.model.{Model, ModelFactory, Resource, RDFNode, RDFList}
+import org.apache.jena.rdf.model.{Model, ModelFactory, Resource, RDFNode, RDFList, SimpleSelector}
 import org.apache.jena.util.FileUtils
 import org.topbraid.jenax.util.JenaUtil
 import org.topbraid.jenax.util.SystemTriples
@@ -21,63 +21,59 @@ import org.topbraid.shacl.vocabulary.SH
 import org.topbraid.shacl.vocabulary.TOSH
 import org.apache.jena.vocabulary.RDF
 import org.apache.jena.vocabulary.RDFS
+import org.rogach.scallop._
 
 import com.typesafe.scalalogging.LazyLogging
 import com.github.tototoshi.csv.CSVReader
 import scala.collection.JavaConverters;
 
-/**
+/*
  * Better validation errors for SHACL
  */
 
-object ValidationErrorPrinter {
-  class ValidationResultWrapper(
-    shapesModel: OntModel,
-    dataModel: OntModel,
-    result: ValidationResult
-  ) {
-    val focusNode = result.getFocusNode
-    val focusNodeClass = {
+case class ValidationError(
+  report: ValidationReport,
+  result: ValidationResult,
+  classNode: RDFNode,
+  focusNode: RDFNode,
+  path: String,
+  sourceConstraintComponent: Resource,
+  message: String,
+  value: Option[RDFNode]
+)
+
+object ValidationErrorGenerator {
+  def generate(report: ValidationReport, shapesModel: OntModel, dataModel: OntModel): Seq[ValidationError] = {
+    val results = JavaConverters.asScalaBuffer(report.results).toSeq
+
+    // 1. Group results by source shape.
+    val resultsByClass = results.groupBy({ result =>
+      val focusNode = result.getFocusNode
       val statement = dataModel.getProperty(focusNode.asResource, RDF.`type`)
-      if (statement == null) RDFS.Class else statement.getObject
-    }
-    val path = result.getPath
-    val value = result.getValue
-    val message = result.getMessage
 
-    val summarizedPath = summarizeResource(path)
-
-    override def toString(): String = {
-      if (value == null) message else s"${value}: ${message}"
-    }
-  }
-
-  def wrapResults(report: ValidationReport, shapesModel: OntModel, dataModel: OntModel) =
-    JavaConverters.asScalaBuffer(report.results).toSeq.map(new ValidationResultWrapper(shapesModel, dataModel, _))
-
-  def print(results: Seq[ValidationResultWrapper]): Unit = {
-    // 1. Group results by focus node class.
-    val resultsByClass = results.groupBy(_.focusNodeClass)
-    resultsByClass.toSeq.sortBy(_._2.size).foreach({ case (classNode, classResults) =>
-      println(s"Class has ${classResults.size} errors: $classNode")
-
-      // 2. Group results by target node.
-      val resultsByFocusNode = classResults.groupBy(_.focusNode)
-      resultsByFocusNode.toSeq.sortBy(_._2.size).foreach({ case (focusNode, focusNodeResults) =>
-        println(s" - Focus node has ${focusNodeResults.size} errors: $focusNode")
-
-        val resultsByPath = focusNodeResults.groupBy(_.path)
-        resultsByPath.toSeq.sortBy(_._2.size).foreach({ case (pathNode, pathNodeResults) =>
-          println(s"   - ${summarizeResource(pathNode)}")
-          pathNodeResults.foreach(result => {
-            println(s"     - ${result.toString}")
+      if (statement == null) RDFS.Class
+      else statement.getObject
+    })
+    resultsByClass.toSeq.sortBy(_._2.size).flatMap({ case (classNode, classResults) =>
+      val resultsByFocusNode = classResults.groupBy(_.getFocusNode)
+      resultsByFocusNode.toSeq.sortBy(_._2.size).flatMap({ case (focusNode, focusNodeResults) =>
+        val resultsByPath = focusNodeResults.groupBy(_.getPath)
+        resultsByPath.toSeq.sortBy(_._2.size).flatMap({ case (pathNode, pathNodeResults) =>
+          pathNodeResults.map(result => {
+            ValidationError(
+              report,
+              result,
+              classNode,
+              focusNode,
+              summarizeResource(pathNode),
+              result.getSourceConstraintComponent,
+              result.getMessage,
+              if (result.getValue == null) None else Some(result.getValue)
+            )
           })
         })
       })
-      println()
     })
-
-    println(s"FAIL ${results.size} failures across ${resultsByClass.keys.size} classes.")
   }
 
   def summarizeResource(node: RDFNode): String = {
@@ -95,14 +91,44 @@ object ValidationErrorPrinter {
 }
 
 /**
+ * Command line configuration for Validate.
+ */
+class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+  val shapes = trailArg[File](
+    descr = "Shapes file to validate (in Turtle)"
+  )
+  val data = trailArg[File](
+    descr = "Data file to validate (in Turtle)"
+  )
+  val only = opt[List[String]](
+    default = Some(List()),
+    descr = "Only display SourceConstraintComponent ending with these strings"
+  )
+  val ignore = opt[List[String]](
+    default = Some(List()),
+    descr = "Don't display SourceConstraintComponent ending with these strings"
+  )
+  val displayNodes = opt[Boolean](
+    default = Some(false),
+    descr = "Display all failing nodes as Turtle"
+  )
+  verify()
+}
+
+/**
  * Use the given shapes file to validate the given data file.
  * This reads the data file and so can provide better error messages that
  * TopBraid's SHACL engine.
  */
 
 object ShacliApp extends App with LazyLogging {
-  val shapesFile = new File(args(0))
-  val dataFile = new File(args(1))
+  // Parse command line arguments.
+  val conf = new Conf(args)
+
+  val shapesFile = conf.shapes()
+  val dataFile = conf.data()
+  val onlyConstraints = conf.only()
+  val ignoreConstraints = conf.ignore()
 
   // Set up the base model.
   val dm = new OntDocumentManager()
@@ -130,8 +156,58 @@ object ShacliApp extends App with LazyLogging {
   engine.validateAll()
   val report = engine.getValidationReport
 
-  if (report.conforms)
+  if (report.conforms) {
     println("OK")
-  else
-    ValidationErrorPrinter.print(ValidationErrorPrinter.wrapResults(report, shapesModel, dataModel))
+    System.exit(0)
+  } else {
+    val errors = ValidationErrorGenerator.generate(report, shapesModel, dataModel)
+
+    def stringEndsWithOneOf(str: String, oneOf: Seq[String]): Boolean =
+      oneOf.exists(str.endsWith(_))
+
+    val filteredErrors = errors
+      .filter(err => onlyConstraints.isEmpty || stringEndsWithOneOf(err.sourceConstraintComponent.toString, onlyConstraints))
+      .filter(err => ignoreConstraints.isEmpty || !stringEndsWithOneOf(err.sourceConstraintComponent.toString, ignoreConstraints))
+
+    filteredErrors.groupBy(_.classNode).foreach({ case (classNode, classErrors) =>
+      // TODO: look up the classNode label
+      println(s"CLASS <${classNode}> (${classErrors.length} errors)")
+      classErrors.groupBy(_.focusNode).foreach({ case (focusNode, focusErrors) =>
+        println(s"Node ${focusNode} (${focusErrors.length} errors)")
+        focusErrors.groupBy(_.path).foreach({ case (path, pathErrors) =>
+          println(s" - Path <${path}> (${pathErrors.length} errors)")
+          pathErrors.foreach(error => {
+            println(s"   - [${error.sourceConstraintComponent}] ${error.message} ${
+              error.value.map(value => s"(value: $value)").mkString(", ")
+            }")
+          })
+          println()
+        })
+        println()
+        if (conf.displayNodes()) {
+          // Display focusNode as Turtle.
+          val focusNodeModel = focusNode.inModel(dataModel).asResource.listProperties.toModel
+          focusNodeModel.setNsPrefixes(JavaConverters.mapAsJavaMap(Map(
+            "SEPIO" -> "http://purl.obolibrary.org/obo/SEPIO_"
+          )))
+
+          val stringWriter = new StringWriter
+          focusNodeModel.write(stringWriter, "Turtle")
+          println(s"Focus node model:\n${stringWriter.toString}")
+        }
+      })
+    })
+
+    println()
+    println(s"${filteredErrors.length} errors displayed")
+
+    val ignoredErrors = errors diff filteredErrors
+    if (!ignoredErrors.isEmpty) {
+      println(s"${ignoredErrors.length} errors ignored because:")
+      onlyConstraints.foreach(only => println(s" - Only displaying sourceConstraintComponents ending in '$only'"))
+      ignoreConstraints.foreach(ignored => println(s" - Ignoring sourceConstraintComponents ending in '$ignored'"))
+    }
+
+    System.exit(1)
+  }
 }
