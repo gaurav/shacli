1df539f0b05719e572492fda327604c9012393c1
Gaurav Vaidya <gaurav@renci.org>
2020-03-18 15:32:19 -0400

    Added support for min and max counts for properties.

--- src/main/scala/org/renci/shacli/generator/Generator.scala
+++ src/main/scala/org/renci/shacli/generator/Generator.scala
@@ -1,25 +1,24 @@
 package org.renci.shacli.generator
 
-import scala.language.reflectiveCalls
-import scala.collection.JavaConverters._
-
-import java.io.{File, BufferedOutputStream, FileOutputStream}
+import java.io.{BufferedOutputStream, File, FileOutputStream}
 
 import com.typesafe.scalalogging.Logger
-import org.apache.jena.rdf.model.{Model, InfModel, ModelFactory}
+import org.apache.jena.rdf.model.{InfModel, Model, ModelFactory, RDFNode, Resource}
 import org.apache.jena.riot.{RDFDataMgr, RDFFormat}
-import org.apache.jena.vocabulary.RDF
+import org.apache.jena.vocabulary.{RDF, RDFS}
+import org.renci.shacli.ShacliApp
 import org.topbraid.shacl.vocabulary.SH
 
-import org.renci.shacli.ShacliApp
+import scala.collection.JavaConverters._
+import scala.language.reflectiveCalls
 
 /**
   * Generate SHACL based on Turtle provided.
   */
 object Generator {
   def generate(logger: Logger, conf: ShacliApp.Conf): Int = {
-    val dataFiles: List[File]  = conf.generate.data()
-    val baseURI: String        = conf.generate.baseURI()
+    val dataFiles: List[File] = conf.generate.data()
+    val baseURI: String = conf.generate.baseURI()
     val outputFilename: String = conf.generate.output()
 
     // Start by loading all data into a single data model.
@@ -36,8 +35,8 @@ object Generator {
     val infModel: InfModel = ModelFactory.createRDFSModel(dataModel)
 
     // Identify every rdf:type in this data model.
-    val typedResources = infModel.listResourcesWithProperty(RDF.`type`).toList.asScala.toSeq
-    val resProps = typedResources
+    val typedResources = infModel.listResourcesWithProperty(RDF.`type`).toList.asScala
+    val resProps: Map[RDFNode, Seq[Resource]] = typedResources
       .flatMap(res => res.listProperties(RDF.`type`).toList.asScala)
       .groupBy(_.getObject)
       .mapValues(_.map(_.getSubject))
@@ -52,17 +51,59 @@ object Generator {
     resProps.keys.foreach(rdfClass => {
       println("Class " + dataModel.shortForm(rdfClass.asResource.getURI))
 
-      val shapeResource =
+      val shapeRes =
         shapesModel.createResource(baseURI + rdfClass.asResource.getLocalName + "Shape")
-      shapeResource.addProperty(RDF.`type`, SH.NodeShape)
-      shapeResource.addProperty(SH.targetClass, rdfClass)
+      shapeRes.addProperty(RDF.`type`, SH.NodeShape)
+      shapeRes.addProperty(SH.targetClass, rdfClass)
 
       val props = resProps
         .getOrElse(rdfClass, Seq())
         .flatMap(_.listProperties.toList.asScala.map(_.getPredicate))
         .toSet
+
       props.foreach(prop => {
-        println(" - Property: " + dataModel.shortForm(prop.getURI))
+        // Set up the shacl:path.
+        val propRes = shapesModel.createResource()
+        propRes.addProperty(
+          SH.path,
+          prop
+        )
+
+        // Find names and descriptions for this property.
+        val labels = prop.listProperties(RDFS.label).toList.asScala.map(_.getLiteral)
+        labels.foreach(literal => propRes.addProperty(
+          SH.name,
+          literal
+        ))
+
+        // Find a description for this property.
+        propRes.addProperty(
+          SH.description,
+          "Enter description here"
+        )
+
+        // What is the minimum and maximum number of times this property is used
+        // per entity?
+        val resourcesInType = resProps.getOrElse(rdfClass, Seq())
+        val propCountPerResource = resourcesInType.map(_.listProperties(prop).toList.size)
+
+        val minCount: Long = propCountPerResource.min
+        val maxCount: Long = propCountPerResource.max
+
+        propRes.addLiteral(
+          SH.minCount,
+          minCount
+        )
+
+        propRes.addLiteral(
+          SH.maxCount,
+          maxCount
+        )
+
+        shapeRes.addProperty(SH.property, propRes)
+
+        // Report
+        println(s" - Property: ${dataModel.shortForm(prop.getURI)} (min: $minCount, max: $maxCount)")
       })
     })
 
@@ -74,8 +115,8 @@ object Generator {
 
     RDFDataMgr.write(outputStream, shapesModel, RDFFormat.TURTLE_PRETTY)
 
-    if (outputFilename != "-") outputStream.close
+    if (outputFilename != "-") outputStream.close()
 
-    return 0
+    0
   }
 }
